adc976509c556773840073cb0c20a934b11ca5f1
siwony_ <siwon103305@gmail.com>
2021-08-17 16:46:25 +0900

    update singly linked list 스펙 정의

--- src/list/linked/singly/SinglyLinkedList.java
+++ src/list/linked/singly/SinglyLinkedList.java
@@ -5,4 +5,16 @@ public interface SinglyLinkedList {
     void printAll();
 
     void addAtHead(int data);
+
+    void addAtTail(int data);
+
+    void addAtSpecificNode(Node node, int data);
+
+    void deleteAtHead();
+
+    void deleteAtTail();
+
+    void deleteAtSpecificNode(Node node, int data);
+
+    void deleteThisList();
 }
--- src/list/linked/singly/SinglyLinkedListImpl.java
+++ src/list/linked/singly/SinglyLinkedListImpl.java
@@ -12,10 +12,45 @@ public class SinglyLinkedListImpl implements SinglyLinkedList{
         this.head = new Node(data, this.head);
     }
 
+    @Override
+    public void addAtTail(int data) {
+        Node findNode = head;
+        while( findNode.getNextNode() != null)
+            findNode = findNode.getNextNode();
+
+        findNode.updateNext(new Node(data));
+    }
+
+    @Override
+    public void addAtSpecificNode(Node node, int data) {
+        //TODO 특정 노드를 기준으로 노드 추가하기
+    }
+
+    @Override
+    public void deleteAtHead() {
+        //TODO 헤드 노드 삭제하기
+    }
+
+    @Override
+    public void deleteAtTail() {
+        //TODO 꼬리 노드 삭제하기
+    }
+
+    @Override
+    public void deleteAtSpecificNode(Node node, int data) {
+        //TODO 특정 노드 삭제하기
+    }
+
+    @Override
+    public void deleteThisList() {
+        //TODO 이 List 삭제하기
+    }
+
+    @Override
     public void printAll(){
-        Node headNode = head;
-        for(; headNode != null; headNode = headNode.getNextNode()){
-            printNode(headNode);
+        Node findNode = head;
+        for(; findNode != null; findNode = findNode.getNextNode()){
+            printNode(findNode);
         }
     }
 
