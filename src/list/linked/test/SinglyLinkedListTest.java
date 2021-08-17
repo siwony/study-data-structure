ed794a2e951b43a1bee09028790ec320479b18e5
siwony_ <siwon103305@gmail.com>
2021-08-17 17:54:14 +0900

    Add 야메로 SinglyLinkedList를  테스트 할 main class 생성

---
+++ src/list/linked/test/SinglyLinkedListTest.java
@@ -0,0 +1,31 @@
+package list.linked.test;
+
+import list.linked.singly.SinglyLinkedList;
+import list.linked.singly.SinglyLinkedListImpl;
+
+public class SinglyLinkedListTest {
+
+    public static void main(String[] args) {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedListImpl(1);
+
+        singlyLinkedList.addAtHead(12);
+        singlyLinkedList.addAtHead(10);
+        singlyLinkedList.addAtSpecificNode(singlyLinkedList.addAtTail(98), 18);
+        singlyLinkedList.printAll();
+
+        singlyLinkedList.addAtTail(99);
+        singlyLinkedList.printAll();
+
+        singlyLinkedList.deleteAtHead();
+        singlyLinkedList.printAll();
+
+        singlyLinkedList.deleteAtTail();
+        singlyLinkedList.printAll();
+
+        singlyLinkedList.deleteAtSpecificNode(singlyLinkedList.findNodeByNodeData(1));
+        singlyLinkedList.printAll();
+
+        singlyLinkedList.deleteThisList();
+        singlyLinkedList.printAll();
+    }
+}
